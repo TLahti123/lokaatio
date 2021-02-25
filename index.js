05fe785cb2ca84edf6809dddcd9339fffd1a85e1
TLahti123 <lahti.toni@gmail.com>
2021-02-25 11:51:55 +0200

    pretty json test

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get("/hei", (req, res) => {
   res.send("heipparallaa");
 });
 
+app.get("/api/locations", (req, res) => {
+  var jsonStr = '{ "bool": true, "number": 123, "string": "foo bar" }';
+  app.set("json spaces", 4); // number of spaces for indentation
+  res.json(jsonStr);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
