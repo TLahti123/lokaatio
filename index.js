f66a245c75795b7732f1a84683137e028d558ddc
TLahti123 <lahti.toni@gmail.com>
2021-02-25 12:26:11 +0200

    prettify / uglify

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const app = express();
-
+const path = require("path");
+const router = express.Router();
 const port = process.env.PORT || 8080;
 // test
 app.use(express.static("public"));
+app.engine("html", require("ejs").renderFile);
 
 app.get("/hei", (req, res) => {
   res.send("heipparallaa");
 });
 
 app.get("/api/locations", (req, res) => {
-  var jsonStr = '{ "bool": true, "number": 123, "string": "foo bar" }';
-  app.set("json spaces", 4); // number of spaces for indentation
-  res.json(jsonStr);
+  let database = [
+    { id: 1, latitude: 60, longitude: 70 },
+    { id: 2, latitude: 40, longitude: 80 },
+  ];
+  app.set("json spaces", "\t"); // number of spaces for indentation
+
+  database2 = JSON.stringify(database);
+  database = JSON.stringify(database, null, "\t");
+
+  res.render(path.join(__dirname + "/public/index.html"), {
+    database,
+    database2,
+  });
 });
 
 app.listen(port, () => {
